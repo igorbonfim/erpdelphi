03d1f2175340b0cc53b64abad9ac5dc2a4cd6610
Igor Bonfim <igorbonfim88@gmail.com>
2023-08-02 17:44:59 -0300

    Definindo o design da aplicação

---
+++ src/views/View.principal.pas
@@ -0,0 +1,37 @@
+unit View.principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
+
+type
+  TViewPrincipal = class(TForm)
+    pnlTopo: TPanel;
+    btnFechar: TButton;
+    pnlMenu: TPanel;
+    pnlBackPrincipal: TPanel;
+    pnlRodape: TPanel;
+    pnlConteudo: TPanel;
+    pnlLogo: TPanel;
+    procedure btnFecharClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  ViewPrincipal: TViewPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+procedure TViewPrincipal.btnFecharClick(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+end.
