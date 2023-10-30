262d1188ddd33fdc67aedfdbce89661613ebcae9
Igor Bonfim <igorbonfim88@gmail.com>
2023-10-30 18:32:46 -0300

    Desenvolvendo a tela base do sistema

--- ERPCurso.dpr
+++ ERPCurso.dpr
@@ -5,7 +5,8 @@ uses
   View.principal in 'src\views\View.principal.pas' {ViewPrincipal},
   Service.conexao in 'src\services\Service.conexao.pas' {ServiceConexao: TDataModule},
   Service.cadastro in 'src\services\Service.cadastro.pas' {ServiceCadastro: TDataModule},
-  Provider.constants in 'src\providers\Provider.constants.pas';
+  Provider.constants in 'src\providers\Provider.constants.pas',
+  View.base in 'src\views\View.base.pas' {ViewBase};
 
 {$R *.res}
 
@@ -15,5 +16,6 @@ begin
   Application.CreateForm(TViewPrincipal, ViewPrincipal);
   Application.CreateForm(TServiceConexao, ServiceConexao);
   Application.CreateForm(TServiceCadastro, ServiceCadastro);
+  Application.CreateForm(TViewBase, ViewBase);
   Application.Run;
 end.
